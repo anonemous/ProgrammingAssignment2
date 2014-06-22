27dc61377d0749fc4fb80c8b8132179f8c0921f4
anonemous <blobdobguy@yahoo.com>
2014-06-22 22:54:35 +0200

    Improve comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,8 @@
-## Define a special type of matrix that can be inverted and
-## having its inverse stored for serving it faster upon subsequent accesses
+## Define a special type of matrix data structure for inverting a matrix
+## and storing the inverse for faster serving upon subsequent accesses
 
-## Takes as parameter a matrix and returns a list of functions
-## to set and get the values of the matrix and its inverse
+## Takes as parameter a matrix and returns a data structure
+## allowing to set and get the values of both the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
@@ -22,8 +22,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Takes as argument a matrix with functions to get and set the values
-##  of the matrix and its inverse and returns its inverse
+## Takes as argument an instance of the matrix data structure having
+## functions to get and set the values of the matrix and its inverse; 
+## returns its inverse of the matrix passed as an argument,
+## upon subsequent accesses and also stores it for faster serving
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
